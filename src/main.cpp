272eb7276e1a8451f3e1bea0c7f719e7ead12251
Cry1is <zsuzuki@smu.edu>
2021-04-22 14:12:59 -0500

    Added character table creation functionality

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,22 @@ using namespace std;
 
 int main(int argc, char** argv){
     //string file = argv[1];
-    Functions* f = new Functions();
+    //Functions* f = new Functions();
     //f->readFile(file);
     string statement = "((a ^ c) -> (a -> b)) ^ (c v d)";
-    vector<string> statements = f->parseStatement(statement);
-    makeTables(statements);
+    //vector<string> statements = f->parseStatement(statement);
+    //makeTables(statements);
 
-    for (int i = 0; i < statements.size(); i++)
-        cout << statements.at(i) << endl;
+    //for (int i = 0; i < statements.size(); i++)
+    //    cout << statements.at(i) << endl;
+
+    Statement* s = new Statement(statement);
+    vector<char>* thing = new vector<char>();
+    thing->push_back('a');
+    thing->push_back('b');
+    thing->push_back('c');
+    thing->push_back('d');
+    s->createTable(*thing);
+    s->printChars();
 }
 
